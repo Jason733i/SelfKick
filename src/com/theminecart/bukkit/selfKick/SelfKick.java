18cf4c10a2c5f285b1418579e496c562a877227c
Jason733i <uniqueberry@gmail.com>
2011-12-04 10:00:27 -0500

    [Jason733i] Finished setup of basic intellij plugin project.

---
+++ src/com/theminecart/bukkit/selfKick/SelfKick.java
@@ -0,0 +1,16 @@
+package com.theminecart.bukkit.selfKick;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SelfKick extends JavaPlugin{
+
+    @Override
+    public void onDisable() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void onEnable() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
